48565318ce93667345f7346ef8b26d241708f512
ASTiNEZh <ASTiNEZh@gmail.com>
2026-02-21 16:19:16 +0300

    First commit

---
+++ src/test/java/ru/astinezh/eBoard/EBoardApplicationTests.java
@@ -0,0 +1,13 @@
+package ru.astinezh.eBoard;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class EBoardApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
