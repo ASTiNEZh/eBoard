1016cf9b7368b86f0eba206f64006cc291bf961b
ASTiNEZh <ASTiNEZh@gmail.com>
2026-04-10 11:42:48 +0300

    Final publishing after merge

--- ApiContracts/build.gradle.kts
+++ ApiContracts/build.gradle.kts
@@ -1,14 +1,16 @@
+import org.apache.commons.io.FilenameUtils
 import org.gradle.kotlin.dsl.register
 import org.openapitools.generator.gradle.plugin.tasks.GenerateTask
 
 plugins {
     id("java-library")
+    id("maven-publish")
     id("org.openapi.generator") version "7.3.0"
 }
 
 group = "ru.ASTiNEZh"
 version = "0.0.1-SNAPSHOT"
-description = "ApiContrascts"
+description = "APIContrascts"
 
 repositories {
     mavenCentral()
@@ -23,32 +25,83 @@ dependencies {
 }
 
 val projGroup = group
-sourceSets.filter { it.name != "test" }.forEach { source ->
-    source.resources.forEach {
-        val fileName = it.nameWithoutExtension
-        val generateTask = tasks.register<GenerateTask>(fileName) {
-            generatorName.set("spring")
-            library.set("spring-cloud")
-            inputSpec.set(it.absolutePath.replace("\\", "/"))
-            outputDir.set(layout.buildDirectory.dir("/openapi/$fileName").get().toString())
-            apiPackage.set("$projGroup.controller")
-            modelPackage.set("$projGroup.dto")
-            configOptions.set(
-                mapOf(
-                    "dateLibrary" to "java8",
-                    "interfaceOnly" to "true",
-                    "useJakartaEe" to "true"
-                )
+sourceSets.filter { it.name != "test" && it.name != "common" }.forEach { source ->
+    source.resources.filter { it.isFile }.filter { it.extension in listOf("yaml", "yml", "json") }.forEach {
+        val group = "$group"
+        val (artifact, version, description) = getInfoOpenApi(it)
+        processingOpenApi(file = it, groupId = group, artifactId = artifact, version = version)
+    }
+}
+
+fun getInfoOpenApi(file: File): Triple<String, String, String> {
+    val openapi = io.swagger.v3.parser.OpenAPIV3Parser().read(file.absolutePath)
+    val description = openapi.info.description
+    val version = if (!openapi.info.version.isNullOrBlank()) openapi.info.version else "$version"
+    val packageName = file.nameWithoutExtension
+    return Triple(packageName, version, description)
+}
+
+fun processingOpenApi(file: File, groupId: String, artifactId: String, version: String) {
+    val fileName = file.nameWithoutExtension
+    val generateTask = tasks.register<GenerateTask>("generate_$groupId-$artifactId-$version") {
+        generatorName.set("spring")
+        library.set("spring-cloud")
+        inputSpec.set(file.absolutePath.replace("\\", "/"))
+        outputDir.set(layout.buildDirectory.dir("/openapi/$fileName").get().asFile.absolutePath)
+        apiPackage.set("$projGroup.controller")
+        modelPackage.set("$projGroup.dto")
+        configOptions.set(
+            mapOf(
+                "dateLibrary" to "java8",
+                "interfaceOnly" to "true",
+                "useJakartaEe" to "true"
             )
-        }
+        )
+    }
+    val pathFromRoot = file.absolutePath
+    val fileNameList = FilenameUtils.getPathNoEndSeparator(pathFromRoot).split(File.separator)
+    println("File paths $fileNameList")
 
-        tasks.named("compileJava") {
-            dependsOn(generateTask)
-        }
 
-        sourceSets.main {
-            java.srcDir("${layout.buildDirectory.get().toString()}/openapi/$fileName/src/main/java")
+    val compileTask = tasks.register("compile_$groupId-$artifactId-$version", JavaCompile::class.java) {
+        this.group = "openapi compilation"
+        this.source = files(layout.buildDirectory.dir("/openapi/$fileName/src").get().asFile).asFileTree
+        this.destinationDirectory.set(layout.buildDirectory.dir("classes/$fileName").get().asFile)
+        this.classpath = sourceSets.getByName("main").compileClasspath
+
+        dependsOn(generateTask)
+    }
+
+    val packageTask = tasks.register("package_$groupId-$artifactId-$version", Jar::class.java) {
+        group = "openapi packaging"
+        archiveVersion.set(version)
+        archiveBaseName.set(fileName)
+        from(layout.buildDirectory.dir("classes/$fileName").get().asFile)
+
+        dependsOn(compileTask)
+    }
+
+    tasks.named("jar") {
+        dependsOn(packageTask)
+    }
+
+    publishing {
+        publications {
+            create<MavenPublication>("${groupId}_${artifactId}_$fileName") {
+                this.groupId = groupId
+                this.artifactId = fileName
+                this.version = version
+                artifact(packageTask.get())
+                repositories {
+                    mavenLocal()
+                }
+            }
         }
     }
 }
 
+afterEvaluate {
+    tasks.named("build") {
+        dependsOn("publishToMavenLocal")
+    }
+}
